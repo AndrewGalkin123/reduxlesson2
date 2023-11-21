48a26289d69b55ccb8067204d89c6dd56a4c886b
AndrewGalkin123 <127788127788a@gmail.com>
2023-11-21 20:52:45 +0200

    styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import React from "react";
+import styled from 'styled-components'
+import Title from "./components/Title";
+import Flex from "./components/Flex";
+import Console from "./components/Console";
+import Button from "./components/Button";
+
+const AppWrapper = styled.div`
+width:100%;
+min-height:100vh;
+padding: 2rem;
+background: black;
+color: green;
+`
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <AppWrapper className="App">
+      <Flex justify="center">
+        <Title color={"green"}>Console cmd 2023</Title>
+      </Flex>
+      <Flex direction="column">
+        <Console />
+        <Button color="green" outlined align="flex-end">Отправить</Button>
+      </Flex>
+
+    </AppWrapper>
   );
 }
 
